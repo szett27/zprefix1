8862a33b522830184796ce976323bc933c2449e5
Scott <scott.zetterstrom@gmail.com>
2022-09-09 16:44:03 -0400

    seperated server and client, started working to get server funcitional

---
+++ server/server.js
@@ -0,0 +1,28 @@
+//Server to connect with backend DB
+//elephant SQL Back End db d2dbe75f-08ed-4221-8a16-c9ce748e1688 
+
+const port = process.env.port || 5000;
+const Pool = require('pg').Pool;
+
+//need to change for actual Database
+const pool = new Pool({
+    connectionString: "postgres://wbhedtqi:k2ptT8uttDODQxz9J4U8INtpvUkIvw5i@jelani.db.elephantsql.com/wbhedtqi",
+    max: 2
+    // user: 'me',
+    // host: 'localhost',
+    // database: 'users',
+    // password: '!QAZxsw2',
+    // port: 5432,
+})
+
+//user login
+
+
+n
+
+
+
+
+//need to setup routes to comply with user stories
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
