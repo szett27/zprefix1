ae9dc30dcc3a49e33beb768736fd33d1726a734e
Scott <scott.zetterstrom@gmail.com>
2022-09-09 19:33:35 -0400

    get routes working in server and get retrieve users and inventory

--- server/server.js
+++ server/server.js
@@ -1,25 +1,38 @@
 //Server to connect with backend DB
 //elephant SQL Back End db d2dbe75f-08ed-4221-8a16-c9ce748e1688 
-
+const express = require('express');
+const app = express();
 const port = process.env.port || 5000;
 const Pool = require('pg').Pool;
 
 //need to change for actual Database
 const pool = new Pool({
     connectionString: "postgres://wbhedtqi:k2ptT8uttDODQxz9J4U8INtpvUkIvw5i@jelani.db.elephantsql.com/wbhedtqi",
-    max: 2
-    // user: 'me',
-    // host: 'localhost',
-    // database: 'users',
-    // password: '!QAZxsw2',
-    // port: 5432,
 })
 
-//user login
+app.use(express.json())
+//get all users
+app.get("/users", async(req, res)=>{
+    try{
+        const allUsers =  await pool.query("SELECT * FROM users");
+        res.json(allUsers.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
 
+//get inventory
+app.get("/inventory", async(req, res)=>{
+    try{
+        const allInventory =  await pool.query("SELECT * FROM inventory");
+        res.json(allInventory.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
 
-n
-
+//delete item
+app.delete("/i")
 
 
 
